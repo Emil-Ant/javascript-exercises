dff56f89803075c7d92303ca195d8c8f4274d615
Emil-Ant <emiliosantoun411@gmail.com>
2022-05-21 16:34:12 +0300

    Added my version

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+let chars = input.split(""); 
+let reverseChars = chars.reverse(); 
+let finalChars = reverseChars.join("");
+return finalChars;  
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
