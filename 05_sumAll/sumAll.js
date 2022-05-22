0cf82b91a8da843d5b714915ebae01c82a45f010
Emil-Ant <emiliosantoun411@gmail.com>
2022-05-22 19:06:43 +0300

    Added my version

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(startInt, finishInt) {
+let sum = 0; 
+for (let i = startInt; i <= finishInt; i++) { 
+    sum += i; 
+}
+return sum 
 };
 
 // Do not edit below this line
