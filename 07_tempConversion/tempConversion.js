6221c0be66aec98d88c0c1886cc4152cc2d27ae5
Emil-Ant <emiliosantoun411@gmail.com>
2022-05-23 23:31:46 +0300

    added my solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+  parseInt(fahrenheit);
+  calctoc = (fahrenheit - 32) * (5 / 9);
+  roundedtoc = Math.round(calctoc * 10) / 10;
+  return roundedtoc; 
 
 };
 
-const ctof = function() {
+const ctof = function(celcius) {
+  parseInt(celcius); 
+  calctof = (celcius * 1.8 + 32);
+  roundedtof = Math.round(calctof * 10) / 10
+  return roundedtof; 
 
 };
 
