43ce49e333a00cf95e8d23004b66e5a202bb8d9a
Emil-Ant <emiliosantoun411@gmail.com>
2022-07-04 08:35:05 +0300

    My versions

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldestPerson = people.reduce((oldest, person) => { 
+      if ((person.yearOfDeath - person.yearOfBirth) > (oldest.yearOfDeath - oldest.yearOfBirth)) { 
+          return person; 
+      } else if ((person.yearOfDeath - person.yearOfBirth) <= (oldest.yearOfDeath - oldest.yearOfBirth)) { 
+          return oldest; 
+      }
 
-};
+  }); 
+  return oldestPerson;
+}; 
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
